57ba59e664f443f04f41a9b2c2ee99f4d7b86069
Fahad <205fahadmohammad@gmail.com>
2024-08-12 19:16:01 +0600

    global error handler add

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ async function main() {
 main();
 
 process.on("unhandledRejection", (err) => {
-  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
+  console.log(`unahandledRejection is detected, shutting down...`, err);
   if (server) {
     server.close(() => {
       process.exit(1);
@@ -30,6 +30,6 @@ process.on("unhandledRejection", (err) => {
 });
 
 process.on("uncaughtException", () => {
-  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  console.log(`uncaughtException is detected, shutting down...`);
   process.exit(1);
 });
